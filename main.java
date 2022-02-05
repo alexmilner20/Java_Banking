a027ce2b8e2f455e524aa25f24c56c47aed0190c
Alex <62556492+alexmilner20@users.noreply.github.com>
2022-02-05 20:28:34 +0000

    update main.java
    starting the main piece

--- main.java
+++ main.java
@@ -1,11 +1,33 @@
-class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-	public static void main(String[] args) {
-		one();	
-	}
+public class Main {
 
-	public String one(){
-		return "hi";
-	}
+  double balance;
 
+  public static void main(String[] args) {
+    banking();
+    
+  }
+
+  static void banking(){
+
+    try {
+          // Enter data using BufferReader
+            BufferedReader reader = new BufferedReader(
+              new InputStreamReader(System.in));
+   
+          // Reading data using readLine
+          System.out.println("Insert number:");
+          String name = reader.readLine();
+
+          double d = Double.parseDouble(name);
+          double d1 = d + 10;
+   
+          // Printing the read line
+          System.out.println("Copy: " + d1);
+    } catch (Exception e) {
+
+    }
+  }
 }
