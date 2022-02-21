51b7fe75a7a33e3cabf78f6eed10fecc023575ff
Alex <62556492+alexmilner20@users.noreply.github.com>
2022-02-21 18:29:15 +0000

    update main.java
    updating with more levels to it.

--- main.java
+++ main.java
@@ -21,11 +21,25 @@ public class Main {
           System.out.println("Insert number:");
           String name = reader.readLine();
 
+          // no income tax or national insurance
           double d = Double.parseDouble(name);
-          double d1 = d + 10;
+          if (d <= 12570 && d < 9564) {
+            System.out.println("You don't pay any tax");
+          }
+          else if (d <= 12570 && d >= 9564 && d <= 50268){
+            double first_ni = (d - 9564) * 0.12;
+            double calc_first_ni = d - first_ni;
+            System.out.println("Amount: £" + calc_first_ni);
+          }
+          else if (d > 12570 || d < 50270){
+            double calc20 = (d - 12570) * 0.2;
+            double calc20f = d - calc20;
+            System.out.println("Amount: £" + calc20f);
+
+          }
    
           // Printing the read line
-          System.out.println("Copy: " + d1);
+          System.out.println("Amount: £" + d);
     } catch (Exception e) {
 
     }
