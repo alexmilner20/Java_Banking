07f2886411fd3ee7eaab9946badcf6c60ec2a8b6
Alex <62556492+alexmilner20@users.noreply.github.com>
2022-03-01 19:31:26 +0000

    Update main.java

--- main.java
+++ main.java
@@ -39,12 +39,14 @@ public class Main {
             double calc_final = (d - first_income) - first_ni;
             System.out.println("Amount: £" + calc_final);
           }
-          // NI & Income tax between 502679 & 150,000 
-          else if (d > 12570 && d >= 9564 && d <= 50268){
-            double first_ni = (d - 9564) * 0.12;
-            double first_income = (d - 12570) * 0.20;
-            double calc_final = (d - first_income) - first_ni;
-            System.out.println("Amount: £" + calc_final);
+          // NI & Income tax between 50269 & 150,000 
+          else if (d > 50269 &&  d <= 150000){
+            double ni_initial = d - 9567;
+            double ni_first = 0.12 * ni_initial;
+            double ni_second = (d - ni_first) * 0.02;
+            double first_income = (d - 12570) * 0.40;
+            double calc_final = (d - first_income) - ni_second;
+            System.out.println("Amount: £" + first_income);
           }
    
           // Printing the read line
