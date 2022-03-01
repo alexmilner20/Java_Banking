995e232431479e7c3947b30c4bdb52f5ab1c6e62
Alex <62556492+alexmilner20@users.noreply.github.com>
2022-03-01 18:20:15 +0000

    update main
    adding layers of the income tax calculator

--- main.java
+++ main.java
@@ -23,19 +23,28 @@ public class Main {
 
           // no income tax or national insurance
           double d = Double.parseDouble(name);
-          if (d <= 12570 && d < 9564) {
+          if (d <= 9564) {
             System.out.println("You don't pay any tax");
           }
+          // National insurance only - under over 9564 but under 12570
           else if (d <= 12570 && d >= 9564 && d <= 50268){
             double first_ni = (d - 9564) * 0.12;
             double calc_first_ni = d - first_ni;
             System.out.println("Amount: £" + calc_first_ni);
           }
-          else if (d > 12570 || d < 50270){
-            double calc20 = (d - 12570) * 0.2;
-            double calc20f = d - calc20;
-            System.out.println("Amount: £" + calc20f);
-
+          // NI & Income tax between 12570 & 50268
+          else if (d > 12570 && d >= 9564 && d <= 50268){
+            double first_ni = (d - 9564) * 0.12;
+            double first_income = (d - 12570) * 0.20;
+            double calc_final = (d - first_income) - first_ni;
+            System.out.println("Amount: £" + calc_final);
+          }
+          // NI & Income tax between 502679 & 150,000 
+          else if (d > 12570 && d >= 9564 && d <= 50268){
+            double first_ni = (d - 9564) * 0.12;
+            double first_income = (d - 12570) * 0.20;
+            double calc_final = (d - first_income) - first_ni;
+            System.out.println("Amount: £" + calc_final);
           }
    
           // Printing the read line
